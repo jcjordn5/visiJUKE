f52f23f108c7c611fd084d454bb95b33d8c49705
Joseph Jordan <jcjordn5@gmail.com>
2015-08-05 11:51:25 -0400

    doing heroku stuff before heartaches

--- lib/soundcloud.rb
+++ lib/soundcloud.rb
@@ -5,7 +5,7 @@ class SoundcloudApi
 		@sounds = []
 
 		@categories = ['alternative+rock','ambient','classical','country','dance+&+edm','dancehall','deep+house','disco','drum+&+bass','dubstep','electronic','folk+&+singer-songwriter','hip+hop+&+rap','house','indie','jazz+&+blues','latin','metal','piano','pop','r&b+&+soul','reggae','reggaeton','rock','soundtrack','techno','trance','techno','trap','trip+hop']
-		spull = HTTParty.get('https://api-v2.soundcloud.com/explore/'+@categories.sample+'?limit==10&offset=0&client_id='+ENV['SoundCloudKey'])
+		spull = HTTParty.get('https://api-v2.soundcloud.com/explore/'+ @categories.sample +'?limit==10&offset=0&client_id='+ENV['SoundCloudKey'])
 		# sclient = SoundCloud.new(:client_id => ENV['SoundCloudKey'])
 
 		spull["tracks"].each do |x|
