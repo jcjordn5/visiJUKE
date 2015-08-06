bd4878fcc23597ace57cae99515917fc92b14be7
Joseph Jordan <jcjordn5@gmail.com>
2015-08-06 12:06:11 -0400

    visi

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150805132840) do
+ActiveRecord::Schema.define(version: 20150805200250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "soundcs", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.string "waveform_url"
-    t.string "stream_url"
-    t.string "artwork_url"
+    t.string  "title"
+    t.string  "description"
+    t.string  "waveform_url"
+    t.string  "stream_url"
+    t.string  "artwork_url"
+    t.integer "sid"
   end
 
 end
