bd1b7e6e84f64f8bfc58288350037fc647274050
Joseph Jordan <jcjordn5@gmail.com>
2015-08-07 11:18:41 -0400

    emlim end

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,6 @@ module Project4
         origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
-    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
